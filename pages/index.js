3c310ad2f64ea95f80ee2c94ff5dfe9b030ae2cf
santipu <santipujolsau@gmail.com>
2022-11-10 12:40:14 +0100

    added gap between NFT cards

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
     return (
         <div className="container mx-auto">
             <h1 className="py-4 px-4 font-bold text-2xl">Recently listed</h1>
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap gap-4">
                 {isWeb3Enabled ? (
                     loading || !listedNfts ? (
                         <div>Loading...</div>
